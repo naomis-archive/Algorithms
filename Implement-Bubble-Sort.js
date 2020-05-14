c418a292ab2883ec844263d5dd9bd48b5b5fe356
Nicholas Carrigan <nhcarrigan@gmail.com>
2020-05-13 21:29:40 -0700

    prettier

--- Implement-Bubble-Sort.js
+++ Implement-Bubble-Sort.js
@@ -1,12 +1,12 @@
 /*Bubble sort works by checking if value is larger than next value, and pushing it toward end*/
 function bubbleSort(array) {
-  for (let iteration = 0; iteration < array.length-1; iteration++)
-    if (array[iteration] > array[iteration+1]) {
-      let temp = array[iteration]
-      array.splice(iteration, 1)
-      array.push(temp)
+  for (let iteration = 0; iteration < array.length - 1; iteration++)
+    if (array[iteration] > array[iteration + 1]) {
+      let temp = array[iteration];
+      array.splice(iteration, 1);
+      array.push(temp);
       //start loop over again
-      iteration = -1
+      iteration = -1;
     }
   return array;
 }
