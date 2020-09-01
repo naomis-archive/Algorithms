d32c6ebafda139472624552ab03947155e3a08b6
nhcarrigan <nhcarrigan@gmail.com>
2020-09-01 15:06:59 -0700

    Templates and workflows

--- CodeWars/TypeScript/Image Host Filename Generator.ts
+++ CodeWars/TypeScript/Image Host Filename Generator.ts
@@ -1,12 +1,10 @@
 //generate a 6 character unique name for an image added to the photoManager
-function generateName()
-{
- let name = ""
- for (let i = 1; i <= 6; i++) {
-   const random = Math.floor(Math.random() * 26) + 65
-   name += String.fromCharCode(random)
- }
-  if (photoManager.nameExists(name)) return generateName()
-  return name
+function generateName() {
+  let name = "";
+  for (let i = 1; i <= 6; i++) {
+    const random = Math.floor(Math.random() * 26) + 65;
+    name += String.fromCharCode(random);
+  }
+  if (photoManager.nameExists(name)) return generateName();
+  return name;
 }
-
