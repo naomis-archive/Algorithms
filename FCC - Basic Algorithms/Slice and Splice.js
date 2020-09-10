3f3889d182f7f0096e1ec996ed636aeaaef5d797
nhcarrigan <nhcarrigan@gmail.com>
2020-09-10 10:02:33 -0700

    Fix Slice and Splice

--- FCC - Basic Algorithms/Slice and Splice.js
+++ FCC - Basic Algorithms/Slice and Splice.js
@@ -1,4 +1,4 @@
 //insert the values of arr1 into arr2 at index n, without modifying original arrays.
 function frankenSplice(arr1, arr2, n) {
-  return [...arr2.splice(0, n), ...arr1, ...arr2];
+  return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
 }
